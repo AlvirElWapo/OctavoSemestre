648a66accf673cbe04f47bab30abdebe6af2d8df
AlvirElWapo <correodeandresalvir@gmail.com>
2023-02-12 16:28:03 -0600

    Ejercicio Procesamiento de imágenes completado

--- Procesamiento_De_Imágenes/Ejercicios_o_Programas/PracticaImagen/practica_espacios_de_color.py
+++ Procesamiento_De_Imágenes/Ejercicios_o_Programas/PracticaImagen/practica_espacios_de_color.py
@@ -1,32 +1,41 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import interactive
+interactive(True)
 
 def Colocar_pantalla(nombre_de_pantalla):
     #------ ACOMODAR LA IMAGEN PARA QUE SE VEA COMPLETA EN MI PANTALLA
     #------ SE DEBE AJUSTAR DEPENDIENDO LA IMÁGEN Y EL TAMAÑO DE PANTALLA
     cv2.moveWindow(nombre_de_pantalla, 10,50)
 
-def Generar_Histograma(imagen, canales, color):
-    hist = cv2.calcHist([imagen], canales, None, [256], [0, 256])
-    plt.plot(hist, color=color);
-    plt.xlim([0, 256])
+def Crear_Histograma(Imagen, numero):
+    plt.hist(Imagen.ravel(), 256, [0,256]);
+    plt.savefig('Histogramas/Histograma' + numero + '.png')
+    plt.close()
+
+def crear_Histograma_RGB(Imagen, numero):
+    color = ('b','g','r')
+    for i,col in enumerate(color):
+        histr = cv2.calcHist([Imagen],[i],None,[256],[0,256])
+        plt.plot(histr,color = col)
+        plt.xlim([0,256])
+    plt.savefig('Histogramas/Histograma' + numero + '.png')
+    plt.close()
 
 
-img = cv2.imread('Imagen3.png', 1)
+img = cv2.imread('Imagen4.jpg', 1)
 cv2.imshow('ventana_generica', img)
 Colocar_pantalla('ventana_generica')
-Generar_Histograma(img,)
 cv2.waitKey(0)
-
-
+crear_Histograma_RGB(img,"1");
 # PRIMER SISTEMA DE COLOR ---> ESCALA DE GRISES
 
 gris = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY);
 cv2.imshow('ventana_generica', gris);
 Colocar_pantalla('ventana_generica')
 cv2.waitKey(0);
-
+Crear_Histograma(gris,"2")
 
 # SEGUNDO SISTEMA DE COLOR ---> XYZ
 
@@ -34,6 +43,7 @@ XYZ = cv2.cvtColor(img, cv2.COLOR_RGB2XYZ);
 cv2.imshow('ventana_generica', XYZ);
 Colocar_pantalla('ventana_generica')
 cv2.waitKey(0);
+crear_Histograma_RGB(XYZ,"3")
 
 
 # TERCER SISTEMA DE COLOR ---> YCrCB
@@ -42,3 +52,4 @@ YCRCB = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb);
 cv2.imshow('ventana_generica', YCRCB);
 Colocar_pantalla('ventana_generica')
 cv2.waitKey(0);
+crear_Histograma_RGB(YCRCB,"4")
